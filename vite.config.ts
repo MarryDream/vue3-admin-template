ff6ee1f1ab93557c095dde11c6d069c9b9f95333
MarryDream <2037177696@qq.com>
2022-03-16 15:10:47 +0800

    build: 生产环境静态资源路径问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import {ConfigEnv, defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -6,32 +6,44 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import * as path from "path"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "./",
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  server: {
-    port: 20715,
-    open: true
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()]
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()]
-    })
-  ],
-  // 关闭charset，解决打包报错 [WARNING] "@charset" must be the first rule in the file
-  css: {
-    preprocessorOptions: {
-      scss: {
-        charset: false
+export default ({ mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd())
+  return defineConfig({
+    base: env.VITE_PUBLIC_PATH,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    server: {
+      port: 20715,
+      open: true
+    },
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ],
+    // 关闭charset，解决打包报错 [WARNING] "@charset" must be the first rule in the file
+    css: {
+      postcss: {
+        plugins: [
+          {
+            postcssPlugin: 'internal:charset-removal',
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === 'charset') {
+                  atRule.remove();
+                }
+              }
+            }
+          }
+        ]
       }
     }
-  }
-})
+  })
+}
